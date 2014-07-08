e2591c7f1afb01c3a575e342a7431ed8c6ff29b6
Chokwanghwan <kwang8992@gmail.com>
2014-07-08 22:47:59 +0900

    1) users/profile.html에 user의 정보를 넘겨주는 home() 2) login 혹은 register 요청이 들어오기 전 세션 상태를 체크하는 before_request() 3) login을 위한 login() 4) 회원가입을 위한 register() 선언!

--- app/users/views.py
+++ app/users/views.py
@@ -1,1 +1,60 @@
-app의 users의 views.py 입니다.
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+from werkzeug import check_password_hash, generate_password_hash
+
+from app import db
+from app.users.forms import RegisterForm, LoginForm
+from app.users.models import users
+from app.users.decorators import requires_login
+
+mod = Blueprint('users', __name__, url_prefix='/users')
+
+@mod.route('/me/')
+@requires_login
+def home():
+	return render_template("users/profile.html", user=g.user)
+
+@mod.before_request
+def before_request():
+	g.user = None
+
+	if 'user_id' in session:
+		g.user = User.query.get(session['user_id'])
+
+@mod.route('/login/', methods=['GET', 'POST'])
+def login():
+	form = LoginForm(request.form)
+	if form.validate_on_submit():
+		user = User.query.filter_by(email.form.email.data).first()
+		if user and check_password_hash(user.password, form.password.data):
+			session['user_id'] = user_id
+			flash('Welcome %s' %user.name)
+			return redirect(url_for('users.home'))
+		flash('Wrong email or password', 'error-message')	
+	return render_template("users/login.html", form=form)
+
+@mod.route('/register/', methods=['GET', 'POST'])
+def register():
+	form = RegisterForm(request.form)
+	if form.validate_on_submit():
+		user = User(name=form.name.data, email=form.email.data, \
+			password=generate_password_hash(form.password.data))
+
+	db.session.add(user)
+	db.session.commit()
+
+	session['user_id'] = user_id
+
+	flash('Thanks for registering')
+		return redirect(url_for('users.home'))
+	return render_template("users/register.html", form=form)
+	
+
+
+
+
+
+
+
+
+
+
