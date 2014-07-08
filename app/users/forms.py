2bbf6d5cab444b073ac92fe5cd15d8ce3ce17cf4
Chokwanghwan <kwang8992@gmail.com>
2014-07-08 21:30:27 +0900

    회원가입을 위한 RegisterForm과 로그인을 위한 LoginForm 추

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,1 +1,18 @@
-app의 users의 forms.py 입니다.
+from flask.ext.wtf import Form, RecaptchaField
+from wtforms import TextField, PasswordField, BooleanField
+from wtforms.validators import Required, EqualTo, Email
+
+class LoginForm(Form):
+  email = TextField('Email address', [Required(), Email()])
+  password = PasswordField('Password', [Required()])
+
+class RegisterForm(Form):
+  name = TextField('NickName', [Required()])
+  email = TextField('Email address', [Required(), Email()])
+  password = PasswordField('Password', [Required()])
+  confirm = PasswordField('Repeat Password', [
+      Required(),
+      EqualTo('password', message='Passwords must match')
+      ])
+  accept_tos = BooleanField('I accept the TOS', [Required()])
+  recaptcha = RecaptchaField()
