3613e6a6bec2c6d53936a286377f8d1b9f1197d7
Chokwanghwan <kwang8992@gmail.com>
2014-07-08 21:10:21 +0900

    User role과 User status 선언

--- app/users/constants.py
+++ app/users/constants.py
@@ -1,1 +1,19 @@
-app의 users의 constants.py 입니다.
+#User role
+ADMIN = 0
+STAFF = 1
+USER = 2
+ROLE = {
+	ADMIN : 'admin',
+	STAFF : 'staff',
+	USER : 'user',
+}
+
+#user status
+INACTIVE = 0
+NEW = 1
+ACTIVE = 2
+STATUS = {
+    INACTIVE: 'inactive',
+    NEW: 'new',
+    ACTIVE: 'active',
+}
