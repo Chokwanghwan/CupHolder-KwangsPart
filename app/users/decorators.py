9b629ecdc0e4ad2cd8ab6c503e63916108089664
Chokwanghwan <kwang8992@gmail.com>
2014-07-08 22:08:30 +0900

    login 상태 체크를 위한 decorated_function 선

--- app/users/decorators.py
+++ app/users/decorators.py
@@ -1,1 +1,12 @@
-app의 users의 decorators.py 입니다.
+from functools import wraps
+
+from flask import g, flash, redirect, url_for, request
+
+def requires_login(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if g.user is None:
+      flash(u'You need to be signed in for this page.')
+      return redirect(url_for('users.login', next=request.path))
+    return f(*args, **kwargs)
+  return decorated_function
