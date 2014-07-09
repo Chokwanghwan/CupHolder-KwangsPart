2b80976b124bc9e32895ee22f801eacbf8028570
Chokwanghwan <kwang8992@gmail.com>
2014-07-10 00:13:32 +0900

    secret key 생성 모듈과 error handler 모듈 작

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,33 @@
-app의 __init__.py 입니다.
+import os
+import sys
+
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+
+def install_secret_key(app, filename='secret_key'):
+	filename = os.path.join(app.instance_path, filename)
+	try:
+		app.config['SECRET_KEY'] = open(filename, 'rb').read()
+	except IOError:
+		print('Error: No secret key. Create it with:')
+		full_path = os.path.dirname(filename)
+		if not os.path.isdir(full_path):
+			print('mkdir -p {filename}'.format(filename=full_path))
+		print('head -c 24 /dev/urandom > {filename}'.format(filename=full_path))
+		sys.exit(1)
+
+if not app.config['DEBUG']:
+	install_secret_key(app)
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'), 404
+
+from app.users.views import mod as usersModule
+app.register_blueprint(usersModule)
