8763f5e6d9075cc7a23c0496c17aac74a0bfb5bb
Chokwanghwan <kwang8992@gmail.com>
2014-07-10 00:12:10 +0900

    ADMINS SECRET_KEY 수정

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 
-ADMINS = frozenset(['youremail@yourdomain.com'])
-SECRET_KEY = 'This string will be replaced with a proper key in production.'
+ADMINS = frozenset(['kwang8992@gmail.com'])
+SECRET_KEY = 'CHOkwanghwan@2'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 DATABASE_CONNECT_OPTIONS = {}
@@ -12,7 +12,7 @@ DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 8
 
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = "somethingimpossibletoguess"
+CSRF_SESSION_KEY = "CHOkwanghwan@2"
 
 RECAPTCHA_USE_SSL = False
 RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
