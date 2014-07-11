27de0ee4c37749b9d7c5a3b44a5a24f0a66df0da
Chokwanghwan <kwang8992@gmail.com>
2014-07-11 11:50:10 +0900

    검색을 담당하는 버튼인 searchCompleteButton 생성 및 onresult 모듈 간략화

--- app/static/js/voiceRecognition.js
+++ app/static/js/voiceRecognition.js
@@ -2,10 +2,12 @@ var MYAPPLICATION = {
 	voiceRecognition : {
 		oRecognition : new webkitSpeechRecognition(),
 		final_transcript : '',
+
 		init:function(){
 			this.oRecognition.continuous = true;
 			this.oRecognition.interimResults = true;
 
+			console.log('final_transcript '+this.final_transcript);
 			this.oRecognition.onresult = this.onresult.bind(this);
 			this.oRecognition.onstart = this.onstart;
 			this.oRecognition.onerror = this.onerror;
@@ -14,20 +16,16 @@ var MYAPPLICATION = {
 			this.oRecognition.lang = 'ko-KR';
 			this.oRecognition.start();
 		},
-	
+
 		onresult:function(event){
 			var interim_transcript = '';
 		    
 			for (var i = event.resultIndex; i < event.results.length; ++i) {
 				if (event.results[i].isFinal) {
-					var url = "/Search?id=" + this.final_transcript;
-					MYAPPLICATION.ajax.run("GET", url, this.searchResult);
 					this.final_transcript += event.results[i][0].transcript;
-					
 					console.log('final_transcript : '+this.final_transcript)
 				} else {
 					interim_transcript += event.results[i][0].transcript;
-					
 					console.log('interim_transcript : '+interim_transcript)
 				}
 				document.querySelector('#test').innerHTML = this.final_transcript;
@@ -41,6 +39,7 @@ var MYAPPLICATION = {
 
 		onerror:function(event){
 			console.log('onerror');
+			console.log(event);
 		},
 
 		onend:function(){
@@ -48,9 +47,8 @@ var MYAPPLICATION = {
 		},
 
 		searchResult : function (){
-			if(this.oXhr.readyState==4){
-				console.log('oXhr.responseText : ' +this.oXhr.responseText)
-				document.querySelector('#voiceButton').innerHTML = '<a href="'+this.oXhr.responseText+'">검색 결과</a>';
+			if(this.oXhr.readyState==4){			
+				console.log('oXhr.responseText : '+this.oXhr.responseText)
 			}
 		}
 	},
@@ -59,7 +57,7 @@ var MYAPPLICATION = {
 		oXhr : '',
 		run : function(method, url, callback){
 			this.oXhr = new XMLHttpRequest();
-
+			
 			this.oXhr.onreadystatechange = callback.bind(this);
 			this.oXhr.open(method, url, true);
 			this.oXhr.send();
@@ -68,9 +66,19 @@ var MYAPPLICATION = {
 
 	start : function(){
 		console.log('Application Start');
-		this.voiceRecognition.init();		
+		this.voiceRecognition.init();
+
+		var searchCompleteButton = document.querySelector("#voiceButton");
+		searchCompleteButton.addEventListener("click", function(event){
+			event.preventDefault();
+			var url = "/search/resultSearch/?id=" + document.querySelector('#test').innerHTML;
+			MYAPPLICATION.ajax.run("GET", url, function (){
+				if(this.oXhr.readyState==4){
+					console.log('oXhr.responseText : '+this.oXhr.responseText);
+				}
+			});
+		}, false);
 	}
 }
-
 
 MYAPPLICATION.start();
