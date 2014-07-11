97048bb7e8f1eba6d4f893295a9edf8692f336eb
Chokwanghwan <kwang8992@gmail.com>
2014-07-11 11:53:24 +0900

    추후 검색 결과를 가공하기 위해서 xml을 JSON으로 변환하는 로직 추가

--- app/search/views.py
+++ app/search/views.py
@@ -1,13 +1,26 @@
 #-*- coding: utf-8 -*-
 from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
 
-voiceSearch= Blueprint('voiceRecognition', __name__, url_prefix='/voiceSearch')
+import urllib2
+import xmltodict
+
+import json
+
+voiceSearch= Blueprint('voiceRecognition', __name__, url_prefix='/search')
  
-@voiceSearch.route("/Search", methods=['GET'])
+@voiceSearch.route("/resultSearch/", methods=['GET'])
 def dic():
 	if request.method == 'GET':
 		rssUrl = 'http://openapi.naver.com/search?key=51627fdc5735c05d3d74506315ad3422&query='+request.args.get('id')+'&display=5&start=1&target=kin&sort=sim'
-		print 'rss url = ' +rssUrl
+		rssUrl = rssUrl.replace(' ', '%20');
+		u = urllib2.urlopen(rssUrl.encode('utf-8'))
+		data = u.read()
+		
+		result = xmltodict.parse(data.decode('utf-8'))
+		real_result = json.dumps(result).decode('utf-8') 
+
+		return real_result
 	else:
-		print 'GET으로 안넘어왔습니다.'
-	return rssUrl
+		data = ''
+		print 'else'
+	return data
